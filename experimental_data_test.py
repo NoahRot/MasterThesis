ee07fa6deb576f3172e05b7f31cba69711822a43
Noah Rotunno <noah.rotunno@epfl.ch>
2026-03-05 23:24:32 +0100

    Update for experimental data treatment

--- experimental_data_test.py
+++ experimental_data_test.py
@@ -5,6 +5,46 @@ init_plt(latex=False)
 create_sns_palette()
 
 t, RF2, U2 = load_load_disp_data("data_test/3pointbending.csv", "experiment")
-treat_experimental_data(t, RF2, U2)
+t, RF2, U2 = treat_experimental_data(t, RF2, U2, 5, True)
+#elastic_end, stiffness, intercept_1 = elastic_region_determination_r2_method(RF2, U2, 60, 0.98, True)
+elastic_end, stiffness, intercept_1 = elastic_region_determination_r2_max(RF2, U2, 10, True)
+
+load = RF2
+disp = U2
+id_computation = -1
+intercept_2 = -stiffness*disp[id_computation] + load[id_computation]
+id_elastic_end = elastic_end
+
+# Define the stiffness curves
+x1 = np.linspace(0, (np.max(load)-intercept_1)/stiffness, 2)
+x2 = np.linspace(-intercept_2/stiffness, disp[id_computation], 2)
+y1 = stiffness*x1 + intercept_1
+y2 = stiffness*x2 + intercept_2
+
+# Define area curves
+x = np.copy(disp)
+y_up = np.copy(load)
+x_c = -intercept_2/stiffness
+idx = np.searchsorted(disp, x_c)
+y_c = y_up[idx-1] + (y_up[idx]-y_up[idx-1])/(x[idx]-x[idx-1])*(x_c-x[idx-1])
+x = np.insert(x, idx, x_c)
+y_up = np.insert(y_up, idx, y_c)
+y_down = stiffness*x + intercept_2
+y_down = np.maximum(y_down, np.zeros_like(y_down))
+
+# Plot the results
+fig = plt.figure()
+ax = fig.subplots()
+ax.plot(disp, load, "bx", label="$L-\Delta$ curve")
+ax.plot(disp[id_computation], load[id_computation], "rx", label="Computation point")
+ax.vlines(disp[id_elastic_end], np.min(load), np.max(load), color="k", linestyles=":", label="End of elastic region")
+ax.plot(x1, y1, "k--", label="Stiffness")
+ax.plot(x2, y2, "k--")
+ax.fill_between(x, y_down, y_up, alpha=0.2, label="$A_{pl}$")
+
+ax.set_xlabel("$\Delta$ [μm]")
+ax.set_ylabel("$L$ [μN]")
+ax.set_title("$L-\Delta$ curve")
+ax.legend()
 
 plt.show()
--- tools/load_disp_tool.py
+++ tools/load_disp_tool.py
@@ -51,6 +51,22 @@ def elastic_region_determination_r2_method(load, disp, min_points : int = 3, r2_
 
     return elastic_end, stiffness, intercept
 
+def compute_A_pl(load, disp, stiffness, intercept_1, index_computation):
+    # Compute area under load-disp curve
+    A_pl = np.trapz(load[:index_computation], disp[:index_computation])
+
+    # Add the rest of the area using stiffness
+    x0 = -intercept_1/stiffness
+    x1 = np.min(disp)
+    y1 = stiffness*x1 + intercept_1
+    A_pl += 0.5*(x1 - x0)*y1
+
+    # Remove the triangle area below the index_computation
+    intercept_2 = -stiffness*disp[index_computation] + load[index_computation]
+    A_pl -= 0.5*(disp[index_computation] + intercept_2/stiffness)*load[index_computation]
+
+    return A_pl
+
 """
 Compute the geometric function for the stress intensity factor computation
 Input:
@@ -92,7 +108,7 @@ def J_integral_el(K : float, nu : float, E : float) -> float:
     return K**2 * (1.0 - nu**2) / E
 
 """
-Compute the elastic J-integral
+Compute the plastic J-integral
 Input:
  - eta_pl : (float) ??? parameters to compute the J-elastic
  - B_N : (float) net thickness
@@ -109,13 +125,13 @@ def J_integral_pl(eta_pl : float, B_N : float, b0 : float, index_computation : i
     intercept_2 = -stiffness*disp[index_computation] + load[index_computation]
 
     # Compute area of plasticity
-    total_area = np.trapz(load, disp)
+    total_area = np.trapz(load[:index_computation], disp[:index_computation])
     A_pl = total_area - 0.5*(disp[index_computation] + intercept_2/stiffness)*load[index_computation]
 
     return eta_pl*A_pl/(B_N*b0)
 
 """
-Load the data load-displacement from an .rpt file
+Load the data load-displacement from an .rpt file or an experiment file
 Input:
  - file : (str) File path and name
  - file_type : (str) Type of the file (abaqus: Abaqus output file, experiment: Experiment output file)
@@ -183,33 +199,121 @@ def load_load_disp_data(file : str, file_type : str):
 
     return t, RF2, U2
 
+def elastic_region_determination_r2_max(load, disp, min_points : int = 10, debug_plot = False):
+    # Use R^2 to find the stiffness
+    r2 = 0.0
+    elastic_end = 0
+    r2_list = []
+
+    # Make the list of the R^2 values
+    for i in range(min_points, len(disp)):
+        # Evaluate slope and compute R^2
+        slope, intercept, r_value, p_value, std_err = sci.stats.linregress(disp[:i], load[:i])
+        r2 = r_value**2
+        r2_list.append(r2)
+
+    # Get the maximum of R^2
+    r2_list = np.array(r2_list)
+    elastic_end = np.argmax(r2_list)
+
+    # Compute the correct values for stiffness and intercept
+    stiffness, intercept, r_value, p_value, std_err = sci.stats.linregress(disp[:elastic_end], load[:elastic_end])
+
+    # Debug figure
+    if debug_plot:
+        fig2 = plt.figure()
+        ax2 = fig2.subplots()
+        ax2.plot(r2_list)
+        ax2.vlines(elastic_end, np.min(r2_list), np.max(r2_list), linestyles="--", colors="red")
+        ax2.set_ylabel("$R^2$")
+        ax2.set_title("$R^2$ value evaluation")
+
+    return elastic_end, stiffness, intercept
+
 """
 Treat experimental data
 """
-def treat_experimental_data(t, RF2, U2, nbr_point_threshold = 20):
-    # Find the index before the loading starts
+def treat_experimental_data(t, RF2, U2, nbr_point_threshold = 5, debug_plot = False):
+    # Compute the std of the "nbr_point_threshold" first points
     mean_begining = np.mean(U2[:nbr_point_threshold])
     std_begining = np.std(U2[:nbr_point_threshold])
     mean_end = np.mean(U2[-nbr_point_threshold:])
     std_end = np.std(U2[-nbr_point_threshold:])
-    index_start = 0
 
-    fig = plt.figure()
-    ax = fig.subplots(2,2)
-    ax[0,0].plot(t, U2)
-    ax[0,0].hlines(mean_begining,                  np.min(t), np.max(t), colors = "black", linestyles="-")
-    ax[0,0].hlines(mean_begining - 5*std_begining, np.min(t), np.max(t), colors = "black", linestyles="--")
-    ax[0,0].hlines(mean_begining + 5*std_begining, np.min(t), np.max(t), colors = "black", linestyles="--")
-    ax[0,0].hlines(mean_end,                  np.min(t), np.max(t), colors = "black", linestyles="-")
-    ax[0,0].hlines(mean_end - 5*std_end, np.min(t), np.max(t), colors = "black", linestyles="--")
-    ax[0,0].hlines(mean_end + 5*std_end, np.min(t), np.max(t), colors = "black", linestyles="--")
-    ax[0,0].set_xlabel("$t$ [s]")
-    ax[0,0].set_ylabel("$\Delta$ [mm]")
+    # Get the index at which the slope begin (need 10 values in a row above mean+5*std)
+    begin_index = 0
+    threshold = mean_begining + 5*std_begining
+    while not np.all(U2[begin_index:begin_index + 10] > threshold):
+        begin_index += 1
 
-    ax[0,1].plot(t, RF2)
-    ax[0,1].set_xlabel("$t$ [s]")
-    ax[0,1].set_ylabel("$L$ [N]")
+        if begin_index - 10 >= len(U2):
+            print("ERROR: Can not find the beginning of the U2 slope")
+            quit()
 
-    ax[1,0].plot(U2, RF2)
-    ax[1,0].set_xlabel("$\Delta$ [mm]")
-    ax[1,0].set_ylabel("$L$ [N]")
+    # Get the index at which the slope stop (need 10 values in a row below mean-5*std)
+    end_index = len(U2) - 1
+    threshold = mean_end - 5*std_end
+    while not np.all(U2[end_index-10:end_index] < threshold):
+        end_index -= 1
+
+        if end_index < 10:
+            print("ERROR: Can not find the ending of the U2 slope")
+            quit()
+
+    # Check the end index with the force
+    force_threshold = 0.4
+    while RF2[end_index] < force_threshold:
+        end_index -= 1
+
+    # Treat the data
+    t_treat = t[begin_index:end_index] - t[begin_index]
+    U2_treat = U2[begin_index:end_index] - U2[begin_index]
+    RF2_treat = RF2[begin_index:end_index]
+
+    # Sort the data to avoid overlapping between the trapezes for area computation
+    sort_idx = np.argsort(U2_treat)
+    U2_treat = U2_treat[sort_idx]       # apply them to both arrays
+    RF2_treat = RF2_treat[sort_idx]     # apply them to both arrays
+
+    # Place first displacement at 0
+    U2_treat -= U2_treat[0]
+
+    # Debug plot
+    if debug_plot:
+        fig = plt.figure()
+        ax = fig.subplots(2,3)
+        ax[0,0].plot(t, U2)
+        ax[0,0].hlines(mean_begining,                  np.min(t), np.max(t), colors = "black", linestyles="-")
+        ax[0,0].hlines(mean_begining - 5*std_begining, np.min(t), np.max(t), colors = "black", linestyles="--")
+        ax[0,0].hlines(mean_begining + 5*std_begining, np.min(t), np.max(t), colors = "black", linestyles="--")
+        ax[0,0].hlines(mean_end,                  np.min(t), np.max(t), colors = "black", linestyles="-")
+        ax[0,0].hlines(mean_end - 5*std_end, np.min(t), np.max(t), colors = "black", linestyles="--")
+        ax[0,0].hlines(mean_end + 5*std_end, np.min(t), np.max(t), colors = "black", linestyles="--")
+        ax[0,0].vlines(t[begin_index], np.min(U2), np.max(U2), colors = "red", linestyles="--")
+        ax[0,0].vlines(t[end_index], np.min(U2), np.max(U2), colors = "red", linestyles="--")
+        ax[0,0].set_xlabel("$t$ [s]")
+        ax[0,0].set_ylabel("$\Delta$ [mm]")
+
+        ax[0,1].plot(t, RF2)
+        ax[0,1].vlines(t[begin_index], np.min(RF2), np.max(RF2), colors = "red", linestyles="--")
+        ax[0,1].vlines(t[end_index], np.min(RF2), np.max(RF2), colors = "red", linestyles="--")
+        ax[0,1].set_xlabel("$t$ [s]")
+        ax[0,1].set_ylabel("$L$ [kN]")
+
+        ax[0,2].plot(U2, RF2)
+        ax[0,2].set_xlabel("$\Delta$ [mm]")
+        ax[0,2].set_ylabel("$L$ [kN]")
+
+        ax[1,0].plot(t_treat, U2_treat, color="green")
+        ax[1,0].set_xlabel("$t$ [s]")
+        ax[1,0].set_ylabel("$\Delta$ [mm]")
+
+        ax[1,1].plot(t_treat, RF2_treat, color="green")
+        ax[1,1].set_xlabel("$t$ [s]")
+        ax[1,1].set_ylabel("$L$ [kN]")
+
+        ax[1,2].plot(U2_treat, RF2_treat, color="green")
+        ax[1,2].set_xlabel("$\Delta$ [mm]")
+        ax[1,2].set_ylabel("$L$ [kN]")
+
+    return t_treat, RF2_treat, U2_treat
